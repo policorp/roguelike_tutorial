88e0ccc0d30b3a0bffb04087445019ca7a4d37ef
policorp <policorp22@yandex.ru>
2018-11-20 18:01:55 +0400

    part 6.2

--- components/ai.py
+++ components/ai.py
@@ -2,6 +2,8 @@ import libtcodpy as libtcod
 
 class BasicMonster:
     def take_turn(self, target, fov_map, game_map, entities):
+        results = []
+
         print('The ' + self.owner.name + ' wonders when it will get to move.')
         monster = self.owner
         if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
@@ -10,4 +12,9 @@ class BasicMonster:
                 monster.move_astar(target, entities, game_map)
                 # monster.move_towards(target.x, target.y, game_map, entities)
             elif target.fighter.hp > 0:
-                print('The {0} insults you! Your ego is damaged!'.format(monster.name))
+                #print('The {0} insults you! Your ego is damaged!'.format(monster.name))
+                #monster.fighter.attack(target)
+                attack_results = monster.fighter.attack(target)
+                results.extend(attack_results)
+
+        return results
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -7,6 +7,7 @@ from components.fighter import Fighter
 from entity import Entity
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
+from render_functions import RenderOrder
 
 class GameMap:
     def __init__(self, width, height):
@@ -102,15 +103,15 @@ class GameMap:
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
-                    fighter_component = Fighter(hp = 10, defence = 0, power = 3)
+                    fighter_component = Fighter(hp = 10, defense = 0, power = 3)
                     ai_component = BasicMonster()
                     monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks = True,
-                                     fighter = fighter_component, ai = ai_component)
+                                     render_order = RenderOrder.ACTOR, fighter = fighter_component, ai = ai_component)
                 else:
-                    fighter_component = Fighter(hp = 16, defence = 1, power = 4)
+                    fighter_component = Fighter(hp = 16, defense = 1, power = 4)
                     ai_component = BasicMonster()
                     monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks = True,
-                                     fighter = fighter_component, ai = ai_component)
+                                     render_order = RenderOrder.ACTOR, fighter = fighter_component, ai = ai_component)
 
                 entities.append(monster)
 
