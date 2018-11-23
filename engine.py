a92ab30664106723c8147810ed693d10b979e9e7
policorp <policorp22@yandex.ru>
2018-11-23 12:43:11 +0400

    part 8.2 - inventory menu

--- engine.py
+++ engine.py
@@ -72,6 +72,7 @@ def main():
     mouse = libtcod.Mouse()
 
     game_state = GameStates.PLAYERS_TURN
+    previous_game_state =  game_state
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
@@ -80,7 +81,7 @@ def main():
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
         render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
-                   screen_height, bar_width, panel_height, panel_y, mouse, colors)
+                   screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
 
         fov_recompute = False
 
@@ -92,6 +93,7 @@ def main():
 
         move = action.get('move')
         pickup = action.get('pickup')
+        show_inventory = action.get('show_inventory')
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
@@ -126,8 +128,15 @@ def main():
             else:
                 message_log.add_message(Message('There is nothing here to pick up', libtcod.yellow))
 
+        if show_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.SHOW_INVENTORY
+
         if exit:
-            return True
+            if game_state == GameStates.SHOW_INVENTORY:
+                game_state = previous_game_state
+            else:
+                return True
 
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
--- input_handlers.py
+++ input_handlers.py
@@ -23,6 +23,8 @@ def handle_keys(key):
 
     if key_char == 'g':
         return {'pickup': True}
+    elif key_char == 'i':
+        return {'show_inventory': True}
 
     if key.vk == libtcod.KEY_ENTER and key.ralt:
         # Alt+Enter: toggle full screen
