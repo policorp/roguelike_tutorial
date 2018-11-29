ba10b6f6b4ec7c9ecac708db6a4756ce80898df3
policorp <policorp22@yandex.ru>
2018-11-29 12:31:53 +0400

    part 10.0 - initialize new game function

--- engine.py
+++ engine.py
@@ -1,78 +1,34 @@
 import libtcodpy as libtcod
 
-from components.fighter import Fighter
-from components.inventory import Inventory
 from death_functions import kill_monster, kill_player
-from entity import Entity, get_blocking_entities_at_location
+from entity import get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
-from game_messages import Message, MessageLog
+from game_messages import Message
 from game_states import GameStates
 from input_handlers import handle_keys, handle_mouse
-from map_objects.game_map import GameMap
-from render_functions import clear_all, render_all, RenderOrder
+from loader_functions.initialize_new_game import get_constants, get_game_variables
+from render_functions import clear_all, render_all
 
 def main():
-    screen_width = 80
-    screen_height = 50
-
-    bar_width = 20
-    panel_height= 7
-    panel_y = screen_height - panel_height
-
-    message_x = bar_width + 2
-    message_width = screen_height - bar_width - 2
-    message_height = panel_height - 1
-
-    map_width = 80
-    map_height = 43
-
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    fov_algorithm = 0
-    fov_light_walls = True
-    fov_radius = 10
-
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
-    colors = {
-        'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150),
-        'light_wall': libtcod.Color(130, 110, 50),
-        'light_ground': libtcod.Color(200, 180, 50)
-    }
-
-    fighter_component = Fighter(hp = 30, defense = 2, power = 5)
-    inventory_component = Inventory(26)
-
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks = True, render_order = RenderOrder.ACTOR,
-                    fighter = fighter_component, inventory = inventory_component)
-    entities = [player]
+    constants = get_constants()
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(constants['screen_width'], constants['screen_height'], constants['window_title'], False)
 
-    con = libtcod.console_new(screen_width, screen_height)
-    panel = libtcod.console_new(screen_width, panel_height)
+    con = libtcod.console_new(constants['screen_width'], constants['screen_height'])
+    panel = libtcod.console_new(constants['screen_width'], constants['panel_height'])
 
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
-                      max_monsters_per_room, max_items_per_room)
+    player, entities, game_map, message_log, game_state = get_game_variables(constants)
 
     fov_recompute = True
 
     fov_map = initialize_fov(game_map)
 
-    message_log = MessageLog(message_x, message_width, message_height)
-
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
-    game_state = GameStates.PLAYERS_TURN
-    previous_game_state =  game_state
+    previous_game_state = game_state
 
     targeting_item = None
 
@@ -80,10 +36,12 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
         if fov_recompute:
-            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+            recompute_fov(fov_map, player.x, player.y, constants['fov_radius'], constants['fov_light_walls'],
+                          constants['fov_algorithm'])
 
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
-                   screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log,
+                   constants['screen_width'], constants['screen_height'], constants['bar_width'],
+                   constants['panel_height'], constants['panel_y'], mouse, constants['colors'], game_state)
 
         fov_recompute = False
 
@@ -116,8 +74,6 @@ def main():
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
                 if target:
-                    #print('You kick the ' + target.name + ' in the shins, much to its annoyance!')
-                    #player.fighter.attack(target)
                     attack_results = player.fighter.attack(target)
                     player_turn_results.extend(attack_results)
                 else:
@@ -183,7 +139,6 @@ def main():
             targeting_cancelled = player_turn_result.get('targeting_cancelled')
 
             if message:
-                #print(message.text)
                 message_log.add_message(message)
 
             if targeting_cancelled:
@@ -192,13 +147,11 @@ def main():
                 message_log.add_message(Message('Targeting cancelled'))
 
             if dead_entity:
-                #pass
                 if dead_entity == player:
                     message, game_state = kill_player(dead_entity)
                 else:
                     message = kill_monster(dead_entity)
 
-                #print(message.text)
                 message_log.add_message(message)
 
             if item_added:
@@ -225,7 +178,6 @@ def main():
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
                 if entity.ai:
-                    #entity.ai.take_turn(player, fov_map, game_map, entities)
                     enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
 
                     for enemy_turn_result in enemy_turn_results:
@@ -233,17 +185,14 @@ def main():
                         dead_entity = enemy_turn_result.get('dead')
 
                         if message:
-                            #print (message.text)
                             message_log.add_message(message)
 
                         if dead_entity:
-                            #pass
                             if dead_entity == player:
                                 message, game_state = kill_player(dead_entity)
                             else:
                                 message = kill_monster(dead_entity)
 
-                            #print(message.text)
                             message_log.add_message(message)
 
                             if game_state == GameStates.PLAYER_DEAD:
